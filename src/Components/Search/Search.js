1022157e12a9f49b05e55d63a08075127af9644b
smriad <skmdriad@gmail.com>
2021-07-04 23:27:44 +0800

    component created

---
+++ src/Components/Search/Search.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Search = () => {
+    return (
+        <div>
+            <p>This is Search</p>
+        </div>
+    );
+};
+
+export default Search;
---
+++ src/Components/SearchResult/SearchResult.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SearchResult = () => {
+    return (
+        <div>
+            <p>this is search result</p>
+        </div>
+    );
+};
+
+export default SearchResult;
